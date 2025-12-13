0cee6948ce75c118fb12d96a0e54f87306e729b6
Alexandre <alexandre.gcms@gmail.com>
2025-12-13 02:00:54 +0000

    Reestrutura API para padrao Vercel (root api directory) e corrige CORS

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// Vercel Serverless Function Entrypoint
-// Precisamos apontar para o arquivo JS compilado
-const app = require('../backend/dist/app').default;
-
-module.exports = app;
---
+++ api/index.ts
@@ -0,0 +1,4 @@
+import app from '../backend/src/app';
+
+export default app;
+
